6e65d42410b10c5c9da332958dcffc39555000f3
Rednam-madhavi <rednammadhavi@gmail.com>
2025-02-15 20:34:01 +0530

    Pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
+import Manager from './Components/Manager'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container bg-red-400">Hello</div>
+      <Navbar />
+      <Manager />
     </>
   )
 }
