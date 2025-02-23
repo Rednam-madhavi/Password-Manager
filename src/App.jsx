82f1fd09c6ad0de85b0f390cc723bae52fc3a570
Rednam-madhavi <rednammadhavi@gmail.com>
2025-02-23 11:36:26 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Manager from './Components/Manager'
 import Footer from './Components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
